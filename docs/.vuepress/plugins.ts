f133cbccdf171a2b903ef4998975662a48afab71
aShu-guo <guochengli97@163.com>
2023-02-02 13:56:26 +0800

    feat:调整插件

--- docs/.vuepress/plugins.ts
+++ docs/.vuepress/plugins.ts
@@ -1,7 +1,7 @@
 // 开启google分析
 // import {googleAnalyticsPlugin} from "@vuepress/plugin-google-analytics";
 // 开启本地搜索
-import {searchPlugin} from "@vuepress/plugin-search";
+// import {searchPlugin} from "@vuepress/plugin-search";
 // 为你的文档网站提供搜索功能
 // import {docsearchPlugin} from '@vuepress/plugin-docsearch'
 
@@ -9,6 +9,6 @@ export default {
     plugins: [
         // googleAnalyticsPlugin({id: 'G-0H3Z4EZZYF'}),
         // docsearchPlugin({}),
-        searchPlugin({}),
+        // searchPlugin({}),
     ]
 }
